b2c84269d5b0cef85b965bb0f934873601b25681
Aimen <aimenhamed@live.com.au>
2021-08-26 17:27:09 +1000

    added controllers & get routes

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Catalog.Repositories;
+using Catalog.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+        // GET /items
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
